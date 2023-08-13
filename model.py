41aebb2ff50f52dff8bb58fe540d35e5cd0032a0
TimurHz <timur.hz@outlook.de>
2023-08-13 22:59:41 +0200

    updated train func

--- model.py
+++ model.py
@@ -36,17 +36,17 @@ def train(net, trainloader, optimizer, epochs, device:str):
             loss.backward()
             optimizer.step()
             
-    def test(net, testloader, device:str):
-        criterion = torch.nn.CrossEntropyLoss()
-        correct, loss = 0, 0.0
-        net.eval()
-        net.to(device)
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data[0].to(device), data[1].to(device)
-                outputs = net(images)
-                loss += criterion(outputs, labels).item()
-                _, predicted = torch.max(outputs.data, 1)
-        accuracy = correct / len(testloader.dataset)
-        return loss, accuracy
-                
+def test(net, testloader, device:str):
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, loss = 0, 0.0
+    net.eval()
+    net.to(device)
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+    accuracy = correct / len(testloader.dataset)
+    return loss, accuracy
+            
